95e8115595581687ef5005c23477cb66b78a121a
haithanhphan1603 <haithanhphan163@gmail.com>
2024-10-26 17:11:05 +0700

    update profile page

---
+++ pages/account/profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <Tabs v-model="tab" class="w-1/2">
+    <TabsList class="grid w-full grid-cols-2">
+      <TabsTrigger value="account">Account</TabsTrigger>
+      <TabsTrigger value="orders">Orders</TabsTrigger>
+    </TabsList>
+
+    <TabsContent value="account">
+      <ProfileForm />
+    </TabsContent>
+  </Tabs>
+</template>
+
+<script setup lang="ts">
+import { Tabs, TabsList, TabsContent, TabsTrigger } from '~/components/ui/tabs'
+import ProfileForm from '~/components/account/ProfileForm.vue'
+const tab = ref('account')
+</script>
