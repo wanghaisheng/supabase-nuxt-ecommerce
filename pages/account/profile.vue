ddef377991fe51d9b1e98dea90e524fb45717e5c
haithanhphan1603 <haithanhphan163@gmail.com>
2024-10-26 18:35:03 +0700

    update logout btn

--- pages/account/profile.vue
+++ pages/account/profile.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="container w-full px-4 py-8">
-    <h2 class="text-3xl text-center font-bold mb-8">Welcome, {{ username }}</h2>
-    <Tabs v-model="tab" class="w-1/2 mx-auto">
-      <TabsList class="grid w-full grid-cols-2">
-        <TabsTrigger value="account">Account</TabsTrigger>
+  <div class="container lg:w-1/2 md:w-3/4 w-full mx-auto px-4 py-8">
+    <h2 class="text-3xl font-bold mb-4 lg:mb-8">Welcome, {{ username }}</h2>
+    <Tabs v-model="tab" class="mx-auto">
+      <TabsList class="grid w-full grid-cols-3">
         <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="address">Address</TabsTrigger>
       </TabsList>
 
       <TabsContent value="account">
@@ -21,17 +22,28 @@
         </Card>
       </TabsContent>
     </Tabs>
+    <Button class="mt-4 px-8" @click="logOut">
+      <span class="mr-2">Logout</span>
+      <LogOut class="w-4 h-4" stroke-width="3"> </LogOut
+    ></Button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Tabs, TabsList, TabsContent, TabsTrigger } from '~/components/ui/tabs'
 import ProfileForm from '~/components/account/ProfileForm.vue'
-const tab = ref('account')
+import { LogOut } from 'lucide-vue-next'
 
 const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+const tab = ref('orders')
 
 const username = computed(() => {
   return `${user.value?.user_metadata.firstName} ${user.value?.user_metadata.lastName}`
 })
+
+const logOut = async () => {
+  await supabase.auth.signOut()
+  navigateTo('/')
+}
 </script>
